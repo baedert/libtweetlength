be884a4aa524264dc2e27609d590c37c7e1e2102
Timm Bäder <mail@baedert.org>
2017-10-05 09:09:22 +0200

    Add COPYING file and license headers
    Fixes #1

--- src/libtweetlength.h
+++ src/libtweetlength.h
@@ -1,3 +1,20 @@
+/*  This file is part of libtweetlength, a Gtk+ linux Twitter client.
+*  Copyright (C) 2017 Timm Bäder
+*
+*  libtweetlength is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  libtweetlength is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with libtweetlength.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __LIBTWEETLENGTH_H__
 #define __LIBTWEETLENGTH_H__
 
