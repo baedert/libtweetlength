c6181598186a2ca65f8ad6de9b67c015724e9540
Timm Bäder <mail@baedert.org>
2017-10-22 20:48:58 +0200

    lib: More link parsing

--- tests/entities.c
+++ tests/entities.c
@@ -816,6 +816,78 @@ links (void)
   g_assert_cmpint (entities[0].length_in_characters, ==, 14);
   g_free (entities);
 
+  entities = tl_extract_entities ("foobar.com?a=b`", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b~", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b/", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 15);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b^", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b\\", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b%", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b|", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 14);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com?a=b|b=c", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 18);
+  g_free (entities);
+
+  entities = tl_extract_entities ("foobar.com/&", &n_entities, NULL);
+  g_assert_cmpint (n_entities, ==, 1);
+  g_assert_nonnull (entities);
+  g_assert (entities[0].type == TL_ENT_LINK);
+  g_assert_cmpint (entities[0].start_character_index, ==, 0);
+  g_assert_cmpint (entities[0].length_in_characters, ==, 11);
+  g_free (entities);
+
   entities = tl_extract_entities ("foobar.com!", &n_entities, NULL);
   g_assert_cmpint (n_entities, ==, 1);
   g_assert_nonnull (entities);
